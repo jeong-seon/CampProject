a1b0c7863fb290efdfc077855c64f7e17df52a57
KimHeeseung <80235924+KimHeeseung@users.noreply.github.com>
2022-06-26 16:50:26 +0900

    khs 2022-06-26

--- camp24/src/main/webapp/resources/js/camp24/memberInfo.js
+++ camp24/src/main/webapp/resources/js/camp24/memberInfo.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
 	$('#dbtn').click(function(){
 		$('#frm').attr('action', '/camp24/member/delMember.cmp');
-		$('#frm').submit();
 		
 		if(confirm('정말 탈퇴합니까?')){
-			// form 태그가 완성이 됬으니 전송한다.
+			
 			$('#frm').submit();
-		} 
+		}
 	});
 });
